1f6b9192b5b09f058a275ec561c19b76fd73fc1a
Leonard Lume <leonard.lume@tktk.ee>
2023-09-25 22:16:10 +0300

    add week4 code

---
+++ Week4/Program.cs
@@ -0,0 +1,11 @@
+﻿
+//for-loop
+//dry-principle - do not repeat yourself
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine("We dont need no education.");
+
+}
+
+Console.WriteLine("Have a nice day");
